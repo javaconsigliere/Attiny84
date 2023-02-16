ff846c7d1da38cd4bce822cfb30f644ecc09a253
javaconsigliere <javaconsigliere@gmail.com>
2023-02-16 08:47:55 -0800

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -53,18 +53,29 @@ CUptime Uptime;
 void setup()
 {   
   // register the I2C commands
+  // Count I2C messages
   CommandManager.add(&I2CMessageCounter);
+  // Simple ping with a counter
   CommandManager.add(&PingCounter);
+  // Get the code version of the controller
   CommandManager.add(&Version);
+  // Return the uptime not really working
+  // inaccuracy maybe caused by the I2C library
   CommandManager.add(&Uptime);
+  // Return the current CPU speed in Hz
   CommandManager.add(&CPUSpeed);
+  // Command the update the I2C address of the controller from 10 to 127
+  // the new I2C address will take place after the restart the controller 
   CommandManager.add(&I2CAddress);
+  // Setting and reading pin  digital or analog value, also provision the pin type
+  // as analog or digital
   CommandManager.add(&PinIO);
+  // Set the analog reference value internel or external and levels
   CommandManager.add(&ARef);
 
 #if defined (__AVR_ATtiny84__) 
-    // set the analog reference to external
-    //analogReference(EXTERNAL);
+    // ATyiney84 support reset
+    // other controller like ATyiney85 does not
     CommandManager.add(&Reset);
     
 #endif
